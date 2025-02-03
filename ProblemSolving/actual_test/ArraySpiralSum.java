c9ca4bee378d23f48c19dc2dc165ab7c335ee287
AidenKooG <k037686@gmail.com>
2025-02-03 23:15:46 +0900

    feat: Update actual test

--- ProblemSolving/actual_test/ArraySpiralSum.java
+++ ProblemSolving/actual_test/ArraySpiralSum.java
@@ -1,9 +1,9 @@
-public class SpiralSum {
+public class ArraySpiralSum {
     public static void main(String[] args) {
         int[][] array = {
             {1, 2, 3, 4},
             {5, 6, 7, 8},
-            {9, 10, 11, 12},
+            {9, 10, 11, 12},`
             {13, 14, 15, 16}
         };
 
